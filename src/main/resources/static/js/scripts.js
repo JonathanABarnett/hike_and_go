6db6a1b2418a666a362e13e043d63864ecfed91b
Jonathan Barnett <joanthan.a.barnett@gmail.com>
2018-04-25 15:21:06 -0500

    Add update button to shopping cart

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,2 +1,8 @@
 
 
+$(document).ready(function () {
+   $(".cartItemQty").on('change', function () {
+       var id = this.id;
+       $('#update-item-' + id).css('display', 'inline-block');
+   });
+});
